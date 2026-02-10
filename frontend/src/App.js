dbf6e2e1b6e10ebf1e9924f69af386d3e4d6ccf7
Igor <igorshk271@gmail.com>
2026-02-10 23:55:13 +0200

    Setup project structure

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import './styles/App.css';
+import {useEffect, useState} from "react";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {LanguageProvider} from "./context/LanguageContext";
+import {ThemeProvider} from "./context/ThemeContext";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/test")
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((err) => console.error("Error fetching backend:", err));
+  }, []);
+
+  return (
+      <LanguageProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <h1>{data ? JSON.stringify(data.message) : "Loading..."}</h1>
+          </BrowserRouter>
+        </ThemeProvider>
+      </LanguageProvider>
+  );
+}
+
+export default App;
