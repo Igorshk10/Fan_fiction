5b6aad5415c02d754a29725c2bb6f35dae6b7059
Igor <igorshk271@gmail.com>
2026-02-13 16:38:36 +0200

    third header version

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import style from './header.module.css';
 import { useTranslation } from "react-i18next";
 import {ThemeContext} from "../../context/ThemeContext";
@@ -9,25 +9,34 @@ function Header() {
     const { t } = useTranslation();
     const { theme, toggleTheme } = useContext(ThemeContext);
     const { lang, toggleLanguage } = useContext(LanguageContext);
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
+        // <div className={style.headerWrapper}>
         <header className={style.header}>
-            <h2 className={style.logo}>
-                FanFic
-            <span className={style.accent}>AI</span>
-            </h2>
+            <p className={style.logo}>
+                F<span className={style.accent}>a</span>nF<span className={style.accent}>i</span>c
+
+            </p>
             <hr/>
             <Link className={style.link} to='/'>{t("home")}</Link>
             <Link className={style.link} to='/fanfics'>{t("fanfics")}</Link>
             <div className={style.settings}>
-                <button onClick={toggleLanguage}>{lang.toUpperCase()}</button>
+                <button className={style["button-toggle"]} onClick={toggleLanguage}>{lang === 'en' ? "UA"  : "EN"}</button>
                 <hr/>
-                <button onClick={toggleTheme}>
-                    {theme == 'light' ? <i className='bx bx-sun'></i> : <i className='bx bx-moon'></i>}
+                <button className={style["button-toggle"]} onClick={toggleTheme}>
+                    {theme == 'light' ? <i className='bx bx-moon'></i> : <i className='bx bx-sun'></i>}
                 </button>
+                <button className={style['account-btn']}>{t("account")}</button>
             </div>
-            <button className={style.menu}><i className='bx bx-menu'></i></button>
+            <button className={style.menu} onClick={() => setIsOpen(prev => !prev)}>{isOpen ? <i className='bx bx-x'></i> : <i className='bx bx-menu'></i>}</button>
         </header>
+    // <div className={`${style.mobileMenu} ${isOpen ? style.open : ""}`}>
+    //     <Link className={style["mobile-link"]} to='/'>{t("home")}</Link>
+    //     <Link className={style["mobile-link"]} to='/fanfics'>{t("fanfics")}</Link>
+    //     <button className={style['account-btn']}>{t("account")}</button>
+    // </div>
+    //  </div>
     )
 }
 
